4d14abe26884ea9dd9f97123aa25ba4dcb849116
jamshidsalimov <jamshidsalimov8@gmail.com>
2023-12-25 16:22:57 +0500

    rtg
    erg

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ pathlib.PosixPath = pathlib.WindowsPath
 # title
 st.title('Transportni klassifikatsiya qiluvchi model')
 
-# rasmni joylash
-files=st.file_uploader("Rasm yuklash", type=["avif","png","jpeg","gif","svg"])
-if files:
-    st.image(files) # rasmni chiqarish
-    # PIL convert
-    img=PILImage.create(files)
-    model = load_learner('transport_model.pkl')
+# # rasmni joylash
+# files=st.file_uploader("Rasm yuklash", type=["avif","png","jpeg","gif","svg"])
+# if files:
+#     st.image(files) # rasmni chiqarish
+#     # PIL convert
+#     img=PILImage.create(files)
+#     model = load_learner('transport_model.pkl')
 
-    # Bashorat qiymatni topamiz
-    pred,pred_id, probs =model.predict(img)
-    st.success(f"Bashorat: {pred}")
-    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+#     # Bashorat qiymatni topamiz
+#     pred,pred_id, probs =model.predict(img)
+#     st.success(f"Bashorat: {pred}")
+#     st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
 
-    # plotting
-    fig=px.bar(x=probs*100, y=model.dls.vocab)
-    st.plotly_chart(fig)
+#     # plotting
+#     fig=px.bar(x=probs*100, y=model.dls.vocab)
+#     st.plotly_chart(fig)
